0bd10978171555f062b7e4f97c6ec7ee1b79fe36
Eshita Islam <islam.eshita@gmail.com>
2023-01-22 11:32:17 -0800

    Adding a function to avoid code duplication

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(imageSrc, left, bottom)
+{
+    let imageItem = document.createElement('img')
+    imageItem.src = imageSrc
+    imageItem.style.position = 'fixed'
+    imageItem.style.left = left
+    imageItem.style.bottom = bottom
+    document.body.append(imageItem)
+}
 
-let pineTree = document.createElement('img')
-pineTree.src = "assets/pine-tree.png"
-pineTree.style.position = 'fixed'
-pineTree.style.left = '300px'
-pineTree.style.bottom = '150px'
-document.body.append(pineTree)
+newImage('assets/green-character.gif', '100px', '100px');
+newImage('assets/pine-tree.png', '450px', '200px');
